4150d23e1951f504fbdc5d07f61e71bdcac8cc0c
krishnakarkera14 <karkerakrishna10@gmail.com>
2021-08-03 19:48:06 +0530

    fixing the extra log bugs and colored console.log

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const chokidar = require('chokidar');
 const program = require('caporal');
 const fs = require('fs');
 const { spawn } = require('child_process');
+const chalk = require('chalk');
 
 program
   .version('0.0.1')
@@ -18,8 +19,14 @@ program
       throw new Error(`Could not find the file ${name}`);
     }
 
+    let proc;
     const start = debounce(() => {
-      spawn('node', [name], { stdio: 'inherit' });
+      if (proc) {
+        proc.kill();
+      };
+
+      console.log(chalk.green('>>>>>>STARTING PROCESS'));
+      proc = spawn('node', [name], { stdio: 'inherit' });
     }, 100);
 
     chokidar
